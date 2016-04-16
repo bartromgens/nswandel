317552526dcbb622a7548d57883adbeed5068cb9
Bart Römgens <bart.romgens@gmail.com>
2016-04-16 19:00:57 +0200

    Use the gpx directory to upload gpx files

--- nswandel/trails/models.py
+++ nswandel/trails/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Trail(models.Model):
     title = models.CharField(max_length=300)
-    gpx_file = models.FileField()
+    gpx_file = models.FileField(upload_to='gpx/')
 
     def __str__(self):
         return self.title
