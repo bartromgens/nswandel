4ddde48972d6203863e310eefbe74de7f04a65b1
Bart Römgens <bart.romgens@gmail.com>
2016-04-29 15:04:13 +0200

    Create scraper for groene wissels

--- scraper.py
+++ scraper.py
@@ -66,16 +66,39 @@ urls = [
 
 
 def main():
-    trails_all = Trails()
-    for url in urls:
-        trails = download_extract_gpx(url)
-        for trail in trails:
-            trails_all.append(trail)
+    gwurls = get_groenewissel_urls()
 
-    trails_all.to_json('trails_downloaded.json')
+    for url in gwurls:
+        print(url)
+
+    # trails_all = Trails()
+    # for url in urls:
+    #     trails = download_extract_gpx_wandelzoekpagina(url)
+    #     for trail in trails:
+    #         trails_all.append(trail)
+
+    # trails_all.to_json('trails_downloaded.json')
 
 
-def download_extract_gpx(url):
+def get_groenewissel_urls():
+    urls = []
+    url_table = 'http://nswandel.nl/GW.html'
+    page = requests.get(url_table)
+    tree = lxml.html.fromstring(page.content)
+    rows = tree.xpath("//tbody/tr")
+    counter = 0
+    for row in rows:
+        counter += 1
+        for column in row.iter():
+            if column.tag == 'a':
+                page_url = 'http://nswandel.nl/' + column.attrib['href']
+                print(page_url + ' ' + str(counter) + '/' + str(len(row)))
+                urls.append(page_url)
+                download_extract_gpx_nswandel(page_url)
+    return urls
+
+
+def download_extract_gpx_wandelzoekpagina(url):
     page = requests.get(url)
     tree = lxml.html.fromstring(page.content)
     name = tree.xpath('//h1')[0].text
@@ -89,30 +112,43 @@ def download_extract_gpx(url):
     download_link = download_button[0].getparent()
     if 'href' in download_link.attrib:
         zipurl = download_link.attrib['href']
-        print(zipurl)
-        outdir = 'extract/' + name
-        resp = requests.get(zipurl)
-        if '.zip' in zipurl:
-            print('unzipping')
-            tempzip = open("tempfile.zip", "wb")
-            tempzip.write(resp.content)
-            tempzip.close()
-            zf = ZipFile("tempfile.zip")
-            zf.extractall(path=outdir)
-            zf.close()
-            for root, subFolders, filenames in os.walk(outdir):
-                for filename in fnmatch.filter(filenames, '*.gpx'):
-                    print(os.path.join(root, filename))
-                    shutil.copyfile(os.path.join(root, filename), os.path.join('extract', filename))
-                    trails.append(Trail(name, filename, url))
-        elif '.gpx' in zipurl:
-            filename = os.path.join('extract', name + '.gpx')
-            print('writing gpx to: ' + filename)
-            with open(filename, 'w') as fileout:
-                fileout.write(str(resp.content))
-                trails.append(Trail(name, name + '.gpx', url))
+        extract_zip_file(zipurl, name)
 
-    return trails
+
+def download_extract_gpx_nswandel(url):
+    page = requests.get(url)
+    tree = lxml.html.fromstring(page.content)
+    fonts = tree.xpath('//font[@color="#000000"]')
+    for font in fonts:
+        if font.text == 'GPS-track':
+            gpxurl_relative = font.getparent().attrib['href']
+            gpxurl_absolute = gpxurl_relative.replace('../..', 'http://nswandel.nl')
+            filename = gpxurl_relative.replace('../../gpstracks/', '')
+            print(gpxurl_absolute)
+            print(filename)
+            extract_zip_file(gpxurl_absolute, filename)
+
+
+def extract_zip_file(zipurl, name):
+    outdir = 'extract/' + name
+    resp = requests.get(zipurl)
+    if '.zip' in zipurl:
+        print('unzipping')
+        tempzip = open("tempfile.zip", "wb")
+        tempzip.write(resp.content)
+        tempzip.close()
+        zf = ZipFile("tempfile.zip")
+        zf.extractall(path=outdir)
+        zf.close()
+        for root, subFolders, filenames in os.walk(outdir):
+            for filename in fnmatch.filter(filenames, '*.gpx'):
+                print(os.path.join(root, filename))
+                shutil.copyfile(os.path.join(root, filename), os.path.join('extract', filename))
+    elif '.gpx' in zipurl:
+        filename = os.path.join('extract', name + '.gpx')
+        print('writing gpx to: ' + filename)
+        with open(filename, 'w') as fileout:
+            fileout.write(str(resp.content))
 
 
 if __name__ == "__main__":
