6769768b3b6d2330568199f54134fbaee1a7ed40
Bart Römgens <bart.romgens@gmail.com>
2016-04-29 19:43:11 +0200

    Add function to get the absolute gpx filepath

--- nswandel/trails/models.py
+++ nswandel/trails/models.py
@@ -1,10 +1,17 @@
+import os
+
 from django.db import models
+
+from nswandel.local_settings import MEDIA_ROOT
 
 
 class Trail(models.Model):
     title = models.CharField(max_length=300)
     distance = models.FloatField(blank=True, null=True, help_text='distance in m')
     gpx_file = models.FileField(upload_to='gpx/')
+
+    def get_gpxfilepath(self):
+        return os.path.join(MEDIA_ROOT, self.gpx_file.path)
 
     def __str__(self):
         return self.title
